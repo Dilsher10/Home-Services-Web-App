36de43488799dab373db4f9fc5220c3fde9671cb
dilsher10 <dilsher.dahri10@gmail.com>
2025-01-26 03:48:19 +0500

    changes

--- app/page.js
+++ app/page.js
@@ -1,11 +1,38 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Hero from "./_components/Hero";
+import CategoryList from "./_components/CategoryList";
+import GlobalApi from "./_services/GlobalApi";
+import { useEffect, useState } from "react";
+import BusinessList from "./_components/BusinessList";
 
 export default function Home() {
+
+  const [categoryList, setCategoryList] = useState([]);
+  const [businessList, setBusinessList] = useState([]);
+
+  useEffect(() => {
+    getCategoryList();
+    getAllBusinessList();
+  }, [])
+
+  const getCategoryList = () => {
+    GlobalApi.getCategory().then(res => {
+      setCategoryList(res.categories);
+    })
+  }
+
+  const getAllBusinessList = () => {
+    GlobalApi.getAllBusinessList().then(res=>{
+      setBusinessList(res.businessLists);
+    })
+  }
   return (
     <div>
-      <Hero/>
+      <Hero />
+      <CategoryList categoryList={categoryList}/>
+      <BusinessList businessList={businessList} title={'Popular Business'}/>
     </div>
   );
 }
